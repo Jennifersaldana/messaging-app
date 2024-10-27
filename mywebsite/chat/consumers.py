18970108c1db238ea9118574fceb187b433ad2aa
Jennifer Saldana <jsalda4@lsu.edu>
2024-10-27 18:24:58 -0500

    style and usernames

--- mywebsite/chat/consumers.py
+++ mywebsite/chat/consumers.py
@@ -1,53 +1,42 @@
-'''
-Consumers are the channel's version of Django views except they do more than just respond to requests from the client 
-They initiate requests to the client while keeping an open connection
-'''
-
-## This is what our consumers inherit from 
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
-# Create the 1st consumer and inherit from the websocket consumer object
-# This is called ChatConsumer since it's responsible for incoming messages 
-# from the client and broadcasting out to anyone who has a connection to this consumer in real time
 class ChatConsumer(WebsocketConsumer):
-
-    # Consumers structure our code into a series of functions (connect, receive, disconnect)
-
-    # This is the respond method
-    def connect(self):  # Connect method for the initial request that comes in from the client
-        self.room_group_name = 'test'  # Dynamic value from a user or URL
+    def connect(self):
+        self.room_group_name = 'test'
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()  # Send a message to the client that the connection was made
+        self.accept()
 
-    def receive(self, text_data):  # Receive messages from the client
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        username = text_data_json['username']  # Get the username from the message
         message = text_data_json['message']
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
+                'username': username,  # Include the username in the event
                 'message': message
             }
         )
 
     def chat_message(self, event):
         message = event['message']
+        username = event['username']  # Get the username from the event
 
         self.send(text_data=json.dumps({
-            'type': 'chat',  # Specify the message type for the client
+            'type': 'chat',
+            'username': username,  # Include the username in the message sent to the client
             'message': message
         }))
 
-    # Optionally, you might want to implement a disconnect method
     def disconnect(self, close_code):
-        # Leave the room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9xo0w4nkwx0b%2-t9&uyip&oev9apg8#^m(mpjc)wgo%yr&s@z'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
