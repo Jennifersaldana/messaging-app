6730795b5bf128d23e7455233705d6cc8690684e
Jennifer Saldana <jsalda4@lsu.edu>
2024-10-27 18:17:31 -0500

    simplified

--- mywebsite/chat/consumers.py
+++ mywebsite/chat/consumers.py
@@ -1,29 +1,54 @@
-''''
-Consumers are the channel's version of dijango views execpt they do more than just respond to request from the client 
-Initiate requests to the client while keeping an open connection
+'''
+Consumers are the channel's version of Django views except they do more than just respond to requests from the client 
+They initiate requests to the client while keeping an open connection
 '''
 
 ## This is what our consumers inherit from 
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
-# create 1st consumer and inherit from the websocket consumer object
-# This is called ChatConsumer since its responsible for incoming messages 
+# Create the 1st consumer and inherit from the websocket consumer object
+# This is called ChatConsumer since it's responsible for incoming messages 
 # from the client and broadcasting out to anyone who has a connection to this consumer in real time
 class ChatConsumer(WebsocketConsumer):
 
-# Consumers strcuture our code into a series of functions (connect, recieve, disconnect)
+    # Consumers structure our code into a series of functions (connect, receive, disconnect)
 
-    # this is respond
-    def connect(self): # connect method for the initial requestion that comes in from the client
-        self.accept() # send a message to client that the connection was made
-        self.send(text_data = json.dumps({ # receieve the message from the client side
-            'type' : 'connection_established', #stringified values
-            'message' : 'you are now connected!'
+    # This is the respond method
+    def connect(self):  # Connect method for the initial request that comes in from the client
+        self.room_group_name = 'test'  # Dynamic value from a user or URL
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()  # Send a message to the client that the connection was made
+
+    def receive(self, text_data):  # Receive messages from the client
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'type': 'chat',  # Specify the message type for the client
+            'message': message
         }))
-    
-    # def receive(self, text_data=None, bytes_data=None): # receieve messages from the client
-    #     return super().receive(text_data, bytes_data)
-    
-    # def disconnect(self, code): # disconnect method for handle what happens when a client disconnects from the consumer
-    #     return super().disconnect(code)
+
+    # Optionally, you might want to implement a disconnect method
+    def disconnect(self, close_code):
+        # Leave the room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -44,6 +43,12 @@ INSTALLED_APPS = [
 ]
 
 ASGI_APPLICATION = 'mywebsite.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
